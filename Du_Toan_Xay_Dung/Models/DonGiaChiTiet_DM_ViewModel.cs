4e39720f9c0199d151e2a65274b76e7e2cca3d6c
ThachLinh <nlinh6894@gmail.com>
2016-10-09 19:44:52 +0700

    complete required of  Duy
    ....

--- Du_Toan_Xay_Dung/Models/DonGiaChiTiet_DM_ViewModel.cs
+++ Du_Toan_Xay_Dung/Models/DonGiaChiTiet_DM_ViewModel.cs
@@ -11,13 +11,13 @@ namespace Du_Toan_Xay_Dung.Models
         public DonGiaChiTiet_DM_ViewModel(NormDetail obj)
         {
             ID = obj.ID;
-            NormWork_ID = Convert.ToDecimal(obj.NormWork_ID);
+            NormWork_ID = obj.NormWork_ID;
             UnitPrice_ID = obj.UnitPrice_ID;
             Numbers = obj.Numbers;
         }
 
         public long ID{ get; set; }
-        public decimal NormWork_ID { get; set; }
+        public string NormWork_ID { get; set; }
         public string UnitPrice_ID { get; set; }
         public decimal ?Numbers { get; set; }
     }
